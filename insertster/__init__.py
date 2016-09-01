a06ede801224b7859ac7c0b48f3dd85f7f71ca66
Daniel McDonald <mcdonadt@colorado.edu>
2016-08-31 19:13:01 -0700

    Round 3

--- insertster/__init__.py
+++ insertster/__init__.py
@@ -1,18 +1,27 @@
-from insertster._io import available_parsers
-from insertster._measures import available_score_functions
-from insertster._best import available_best_functions
+from ._io import available_parsers
+from ._measures import available_score_functions
+from ._best import available_best_functions
+from ._core import decorate, propagate, insert, best, score
 
 
-def query_parsers():
-    return list(available_parsers)
+def query_parsers(method=None):
+    if method is None:
+        return list(available_parsers)
+    else:
+        return available_parsers[method]
+
+def score_functions(method=None):
+    if method is None:
+        return list(available_score_functions)
+    else:
+        return available_score_functions[method]
+
+def best_functions(method=None):
+    if method is None:
+        return list(available_best_functions)
+    else:
+        return available_best_functions[method]
 
 
-def score_functions():
-    return list(available_score_functions)
-
-
-def best_functions():
-    return list(available_best_functions)
-
-
-__all__ = ['query_parsers', 'score_functions', 'best_functions']
+__all__ = ['query_parsers', 'score_functions', 'best_functions'
+           'decorate', 'propagate', 'insert', 'best', 'score']
